2a2d3e5ce088092495d3d01cb7c720ebbfbc16ca
BWBMaxime <83752129+BWBMaxime@users.noreply.github.com>
2022-10-19 13:47:47 +0200

    ...

--- Program.java
+++ Program.java
@@ -10,6 +10,7 @@ public class Program
     // Tout Bloc de code doit se trouver dans une méthode
     //La portée se définit en {}
     //Méthode NE PEUT PAS être déclaré hors porté
+    //main n'est pas un mot clef on peut creer un variable "main"
     public static void main(String[] args)
     {
         String messageDePaixPourLeRoiWisigoth = "...Coucou !";

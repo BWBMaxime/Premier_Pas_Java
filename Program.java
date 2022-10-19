531ea118fdb1ac5661b3fc157b6a6bc423945582
BWBMaxime <83752129+BWBMaxime@users.noreply.github.com>
2022-10-19 10:55:59 +0200

    First Program In Java

---
+++ Program.java
@@ -0,0 +1,18 @@
+package Premier_Pas_Java;
+
+public class Program 
+{
+    /**
+     * @param args
+     */
+
+    // Méthode (fonction déclaré dans la classe)
+    // Tout Bloc de code doit se trouver dans une méthode
+    //La portée se définit en {}
+    //Méthode NE PEUT PAS être déclaré hors porté
+    public static void main(String[] args)
+    {
+        String messageDePaixPourLeRoiWisigoth = "...Coucou !";
+        System.out.println(messageDePaixPourLeRoiWisigoth);
+    }
+}
